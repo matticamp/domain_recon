5e711d0d901d88360209b1517e689aaa8cf7482c
matticamp <2312425+matticamp@users.noreply.github.com>
2018-06-13 11:49:24 -0400

    Update domain-recon_win10wsl.bash

--- domain-recon_win10wsl.bash
+++ domain-recon_win10wsl.bash
@@ -1,6 +1,7 @@
 #!/bin/bash
-#Bash script for reconnaissance on domains
-#Created by Mattia Campagnano on Sat Feb 6 00:27:30 EST 2016
+# Bash script for reconnaissance on domains
+# Created by Mattia Campagnano on Sat Feb 6 00:27:30 EST 2016
+# This is a modified version of the original script, intended for use with the Windows Subsystem for Linux under Windows 10
 
 echo "DOMAIN RECON"
 echo "By Mattia Campagnano"
@@ -8,16 +9,16 @@ echo "-------------------"
 echo "Please enter a domain: "
 read domain
 
-#Checks user input and throws an error if no domain was entered.
+# Checks user input and throws an error if no domain was entered.
 
 if [ -z $domain ]; then
   echo -n "Please enter a domain "
   exit 1
 
-#Otherwise executes the recon scripts and opens the website list found below
+# Otherwise executes the recon script and opens the website list found below
 
 else
-   echo -n "recon on $domain";
+   echo "recon on $domain";
 fi
 
 read -p "Press <ENTER > to continue"
@@ -29,13 +30,15 @@ echo "We found the following IP addresses $IPs associated to the $domain domain"
 echo
 echo
 echo
+echo
 
-#Optional: save associated IP addresses to an output file 
-echo "Please enter a filepath for the output file: "
-read output
-echo $IPs > $output
+# Optional: save associated IP addresses to an output file 
 
+#echo "Please enter a filepath for the output file: "
+#read output
+#echo $IPs > $output
 
+# Check the domain entered by the user, prepopulating a list of specific websites and automatically loading multiple Google Chrome tabs
 
 
 /mnt/c/Program\ Files\ \(x86\)/Google/Chrome/Application/chrome.exe -new-tab  http://intodns.com/$domain &
