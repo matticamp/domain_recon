8e3f9b44dc4f4b9efe4dc4e13557026da2934d3d
matticamp <2312425+matticamp@users.noreply.github.com>
2018-06-19 14:44:41 -0400

    Update domain_recon.bash

--- domain_recon.bash
+++ domain_recon.bash
@@ -1,6 +1,6 @@
 #!/bin/bash
 #Bash script for reconnaissance on domains
-#Created by Mattia Campagnano on Sat Feb 6 00:27:30 EST 2016
+#Created by Mattia Campagnano on Tue Jun 19 14:44:07 EDT 2018
 
 
 echo "DOMAIN RECON"
